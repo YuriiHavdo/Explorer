8c437dc378d01f8a3d7e06c6463f90d4c20ccdae
Yurii Havdo <havdoyurii@hotmail.com>
2023-04-10 16:12:05 +0200

    -scroll

--- js/index.js
+++ js/index.js
@@ -1,14 +1,49 @@
-$(".burger_menu").click(function(e) {
-  e.preventDefault()
-  $(this).toggleClass('open');
-  $(".sandwich").toggleClass("active");
-});
 
-$(".burger_menu").click(function () {
-  if ($(".sub-menu").is(":visible")) {
-      $(".sub-menu").fadeOut(600);
-      $(".top_mnu li a").removeClass("fadeInUp animated");
-  } else {
-      $(".sub-menu").fadeIn(600);
-  };
-});
+$('document').ready(function () {
+  var switcher = true
+
+  $(".burger_menu").click(function(e) {
+    e.preventDefault()
+    
+    if(switcher) {
+      $('body').css('overflow', 'hidden');
+      switcher = !switcher
+    } else {
+      $('body').css('overflow', 'initial');
+      switcher = true
+    }
+    $(this).toggleClass('open');
+    $(".sandwich").toggleClass("active");
+  });
+  
+  $(".burger_menu").click(function () {
+    if ($(".sub-menu").is(":visible")) {
+        $(".sub-menu").fadeOut(600);
+        $(".sub-menu li a").removeClass("animated");
+    } else {
+        $(".sub-menu").fadeIn(600);
+        $(".sub-menu li a").addClass("animated")
+    };
+  });
+  
+  console.log($('p.text.second'))
+
+})
+
+
+var user = {
+  name: 'vasil',
+  age: 20,
+  status: 'ukr',
+  getName: function() {
+    return this.name
+  },
+  setName: function(value) {
+    this.name = value
+  }
+};
+
+console.log(user.setName('Bogdan'))
+
+console.log(user.getName())
+
